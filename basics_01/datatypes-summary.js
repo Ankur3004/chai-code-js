b6bb4e13e7c2b4cf51d76f50d612302062f6319f
Ankur Mishra <30298886+Ankur3004@users.noreply.github.com>
2026-03-29 18:21:36 +0000

    completed datatypes revision

---
+++ basics_01/datatypes-summary.js
@@ -0,0 +1,36 @@
+// Primitive 
+
+// 7 Categories : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+let outSideTemp = null
+let userEmail 
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+console.log(id)
+console.log(anotherId)
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["a", "b", "c"]
+
+let myObj = {
+    "name": "Ankur",
+    "age": 20
+
+}
+
+const myFunction = function() {
+    console.log("Hello world")
+}
+
+console.log(typeof bigNumber)
+
